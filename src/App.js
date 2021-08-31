ec4c66564bd011d8f953eb08f83381a15361f9cc
Ian Gongwer <iangongwerpersonal@gmail.com>
2021-08-31 11:11:10 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {useEffect, useState} from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    document.title = `You clicked ${count} times`;
+  });
+
+
+    return (
+      <div class='layout'>
+        <div class='text-button'>
+          <p>You clicked {count} times</p>
+          <button onClick={() => setCount(count + 1)}>Increment by 1</button>
+          </div>
+      </div>
+    );
+  }
 
 export default App;
